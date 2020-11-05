e770cdc7caf523d42877c6f8bb5c970c961aaf13
letsomeerin7 <erinjamison@erins-mbp.home>
2020-11-04 16:30:58 -0800

    I fix it!

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 const customName = document.getElementById('customName');
-const customeNAme = document.getElementById('customeNAme')
+const customeNAme = document.getElementById('customeNAme');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
@@ -8,7 +8,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was a majestic night with a velvety sky covered in twinkling stars and a balmy 75 Fahrenheit. Naturally Abigail and Luke decided it was the perfect time to go star gazing. They gathered some supplies, including :insertX:, before heading to the top of their favorite hill. They got a little carried away and it felt like their bag weighed 100 lbs. But it was worth it because they were able to see so many constellations, even their favorite one :insertY:. In the wee hours of the morning, after seeing :insertZ:, they decided it was finally time to call it a night and go to bed. Hopefully they could go star gazing again soon!';
+let storyText = 'It was a majestic night with a velvety sky covered in twinkling stars and a balmy 75* Fahrenheit. Naturally Abigail and Luke decided it was the perfect time to go star gazing. They gathered some supplies, including :insertX:, before heading to the top of their favorite hill. They got a little carried away and it felt like their bag weighed 100 lbs. But it was worth it because they were able to see so many constellations, even their favorite one :insertY:. In the wee hours of the morning, after seeing :insertZ:, they decided it was finally time to call it a night and go to bed. Hopefully they could go star gazing again soon!';
 let insertX = ['their favorite onesies', 'steaming hot coffee', 'candy, candy, and more candy'];
 let insertY = ['Pegasus', 'Scorpio', 'Cassiopeia'];
 let insertZ = ['a shooting star', 'a comet', 'a meteor shower'];
@@ -16,7 +16,7 @@ let insertZ = ['a shooting star', 'a comet', 'a meteor shower'];
 randomize.addEventListener('click', result);
 
 function result() {
-  let newStory = 'storyText';
+  let newStory = storyText;
 
   let xItem = randomValueFromArray(insertX);
   let yItem = randomValueFromArray(insertY);
@@ -37,15 +37,15 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     let weight = Math.round(100/14) +' stone';
-    let temperature =  Math.round((75-32)*(5/9)) +'* centigrade';
-    newStory = newStory.replace('100 pounds', weight);
-    newStory = newStory.replace('75 Fahrenheit', temperature);
+    let temperature =  Math.round((75-32)*5/9) +'* centigrade';
+    newStory = newStory.replace('100 lbs', weight);
+    newStory = newStory.replace('75* Fahrenheit', temperature);
   }
   if(document.getElementById("fr").checked) {
     let weight = Math.round(100/2.205) +' kg';
-    let temperature =  Math.round((75-32)*(5/9)) +' celsius';
-    newStory = newStory.replace('100 pounds', weight);
-    newStory = newStory.replace('75 Fahrenheit', temperature);
+    let temperature =  Math.round((75-32)*5/9) +'* celsius';
+    newStory = newStory.replace('100 lbs',  weight);
+    newStory = newStory.replace('75* Fahrenheit', temperature);
   }
 
   story.textContent = newStory;
